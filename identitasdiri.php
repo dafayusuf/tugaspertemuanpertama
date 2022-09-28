d15e85a5e0b5ec49ba046c3dc9c64ed095d3ad04
dafayusuf <114553520+dafayusuf@users.noreply.github.com>
2022-09-28 13:13:32 +0700

    Add files via upload

---
+++ identitasdiri.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+           halaman index
+        </title>
+    </head>
+    <body>
+        <?php
+            $nama = "dafa yusuf prasetyo adji";
+            $nim = 21;
+            $alamat = "tegalrejo banyuurip purworejo";
+            $email = "dafayusufprasetyo@gmail.com";
+
+        ?>
+
+        <p>Biodata Diri</p>
+        <p>Nama <?php echo $nama ;?> </p>
+        <p>NIM <?php echo $nim ;?> </p>
+        <p>Alamat <?php echo $alamt ;?> </p>
+        <p>Email <?php echo $email ;?> </p>
+        
+    </body>
+</html>
